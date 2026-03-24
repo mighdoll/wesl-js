781568ecbaa9bcd3b29022ae169c8012eff6399e
lee mighdoll <lee@underneath.ca>
2026-03-24 13:22:44 -0700

    checkpoint

--- examples/pendulum-entropy/vite.config.ts
+++ examples/pendulum-entropy/vite.config.ts
@@ -1,5 +1,13 @@
+import fs from "node:fs";
 import viteWesl from "wesl-plugin/vite";
 
 export default {
   plugins: [viteWesl()],
+  server: {
+    host: true,
+    https: {
+      key: fs.readFileSync("./localhost+1-key.pem"),
+      cert: fs.readFileSync("./localhost+1.pem"),
+    },
+  },
 };
